6e0c93043daacdf16b9bb12556e910f702faae7d
shahroz.khan <shehroz.altaf92>
2022-10-07 17:36:07 +0500

    banners

--- dashboard/resources/views/dashboard/index.blade.php
+++ dashboard/resources/views/dashboard/index.blade.php
@@ -3,13 +3,15 @@
 @section('title', 'Default')
 
 @section('css')
-    <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/animate.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/chartist.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/date-picker.css')}}">
 
+    <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/scrollbar.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/owlcarousel.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/prism.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset(config('global.asset_path').'/css/vendors/whether-icon.css')}}">
+
+
+    <link rel="stylesheet" type="text/css" href="{{asset('public/css/swiper.min.css')}}">
+
+
+
 @endsection
 
 @section('style')
@@ -17,11 +19,82 @@
         .myheight{
             height: 90%;
         }
+        .tales {
+            width: 100%;
+        }
+        .carousel-inner{
+            width:100%;
+            max-height: 200px !important;
+        }
+
+
+        * {box-sizing: border-box;}
+        body {font-family: Verdana, sans-serif;}
+        .mySlides {display: none;}
+        img {vertical-align: middle;}
+
+        /* Slideshow container */
+        .slideshow-container {
+            max-width: 1000px;
+            position: relative;
+            margin: auto;
+        }
+
+        /* Caption text */
+        .text {
+            color: #f2f2f2;
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+        }
+
+        /* Number text (1/3 etc) */
+        .numbertext {
+            color: #f2f2f2;
+            font-size: 12px;
+            padding: 8px 12px;
+            position: absolute;
+            top: 0;
+        }
+
+        /* The dots/bullets/indicators */
+        .dot {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+        }
+
+        .active {
+            background-color: #717171;
+        }
+
+        /* Fading animation */
+        .fade {
+            animation-name: fade;
+            animation-duration: 1.5s;
+        }
+
+        @keyframes fade {
+            from {opacity: .4}
+            to {opacity: 1}
+        }
+
+        /* On smaller screens, decrease text size */
+        @media only screen and (max-width: 300px) {
+            .text {font-size: 11px}
+        }
     </style>
 @endsection
 
 @section('breadcrumb-title')
-    <h3>Default</h3>
+    <h3>Health Facility Punjab</h3>
 @endsection
 
 @section('breadcrumb-items')
@@ -30,95 +103,87 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row second-chart-list third-news-update">
-            <div class="col-xl-4 col-lg-12  morning-sec box-col-12">
-                <div class="card myheight o-hidden profile-greeting">
-                    <div class="card-body">
-                        <div class="media">
-                            <div class="badge-groups w-100">
-                                <div class="badge f-12"><i class="me-1" data-feather="clock"></i><span id="txt"></span>
-                                </div>
-                                <div class="badge f-12"><i class="fa fa-spin fa-cog f-14"></i></div>
+
+
+        <!-- autoplay swiper -->
+        <section id="component-swiper-autoplay">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Autoplay</h4>
+                </div>
+                <div class="card-body">
+                    <div class="swiper-autoplay swiper-container">
+                        <div class="swiper-wrapper">
+                            <?php for($i=1;$i<=10;$i++){ ?>
+                            <div class="swiper-slide">
+                                <img class="img-fluid" src="{{asset('public/slider_imgs/i'.$i.'.jpeg')}}" alt="banner" />
+                            </div>
+                                <?php }?>
+                            <div class="swiper-slide">
+                                <img class="img-fluid" src="{{asset('public/slider_imgs/images/banner/banner-7.jpg')}}" alt="banner" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img class="img-fluid" src="{{asset('public/slider_imgs/images/banner/banner-8.jpg')}}" alt="banner" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img class="img-fluid" src="{{asset('public/slider_imgs/images/banner/banner-9.jpg')}}" alt="banner" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img class="img-fluid" src="{{asset('public/slider_imgs/images/banner/banner-10.jpg')}}" alt="banner" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img class="img-fluid" src="{{asset('public/slider_imgs/images/banner/banner-11.jpg')}}" alt="banner" />
                             </div>
                         </div>
-                        <div class="greeting-user text-center">
-                            <div class="profile-vector"><img class="img-fluid"
-                                                             src="{{asset(config('global.asset_path').'/images/dashboard/welcome.png')}}"
-                                                             alt=""></div>
-                            <h4 class="f-w-600"><span id="greeting">Good Morning</span> <span class="right-circle">{{ Auth::user()->name }} <i
-                                        class="fa fa-check-circle f-14 middle"></i></span> </h4>
-                            <p>
-                                <span> Welcome to {{config('global.project_name')}} Dashboard</span>
-                            </p>
-                        </div>
+                        <!-- Add Pagination -->
+                        <div class="swiper-pagination"></div>
+                        <!-- Add Arrows -->
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
                     </div>
                 </div>
             </div>
+        </section>
+        <!--/ autoplay swiper -->
 
-            <div class="col-xl-4 col-lg-12 calendar-sec box-col-12">
-                <div class="  myheight card mobile-clock-widget">
-                    <div class="">
-                        <div class="bg-svg">
-                            <svg class="climacon climacon_cloudLightningMoon" id="cloudLightningMoon" version="1.1" viewBox="15 15 70 70">
-                                <clippath id="moonCloudFillClipfill11">
-                                    <path d="M0,0v100h100V0H0z M60.943,46.641c-4.418,0-7.999-3.582-7.999-7.999c0-3.803,2.655-6.979,6.211-7.792c0.903,4.854,4.726,8.676,9.579,9.58C67.922,43.986,64.745,46.641,60.943,46.641z"></path>
-                                </clippath>
-                                <clippath id="cloudFillClipfill19">
-                                    <path d="M15,15v70h70V15H15z M59.943,61.639c-3.02,0-12.381,0-15.999,0c-6.626,0-11.998-5.371-11.998-11.998c0-6.627,5.372-11.999,11.998-11.999c5.691,0,10.434,3.974,11.665,9.29c1.252-0.81,2.733-1.291,4.334-1.291c4.418,0,8,3.582,8,8C67.943,58.057,64.361,61.639,59.943,61.639z"></path>
-                                </clippath>
-                                <g class="climacon_iconWrap climacon_iconWrap-cloudLightningMoon">
-                                    <g clip-path="url(#cloudFillClip)">
-                                        <g class="climacon_wrapperComponent climacon_wrapperComponent-moon climacon_componentWrap-moon_cloud" clip-path="url(#moonCloudFillClip)">
-                                            <path class="climacon_component climacon_component-stroke climacon_component-stroke_sunBody" d="M61.023,50.641c-6.627,0-11.999-5.372-11.999-11.998c0-6.627,5.372-11.999,11.999-11.999c0.755,0,1.491,0.078,2.207,0.212c-0.132,0.576-0.208,1.173-0.208,1.788c0,4.418,3.582,7.999,8,7.999c0.614,0,1.212-0.076,1.788-0.208c0.133,0.717,0.211,1.452,0.211,2.208C73.021,45.269,67.649,50.641,61.023,50.641z"></path>
-                                        </g>
-                                    </g>
-                                    <g class="climacon_wrapperComponent climacon_wrapperComponent-lightning">
-                                        <polygon class="climacon_component climacon_component-stroke climacon_component-stroke_lightning" points="48.001,51.641 57.999,51.641 52,61.641 58.999,61.641 46.001,77.639 49.601,65.641 43.001,65.641 "></polygon>
-                                    </g>
-                                    <g class="climacon_wrapperComponent climacon_wrapperComponent-cloud">
-                                        <path class="climacon_component climacon_component-stroke climacon_component-stroke_cloud" d="M59.999,65.641c-0.28,0-0.649,0-1.062,0l3.584-4.412c3.182-1.057,5.478-4.053,5.478-7.588c0-4.417-3.581-7.998-7.999-7.998c-1.602,0-3.083,0.48-4.333,1.29c-1.231-5.316-5.974-9.29-11.665-9.29c-6.626,0-11.998,5.372-11.998,12c0,5.446,3.632,10.039,8.604,11.503l-1.349,3.777c-6.52-2.021-11.255-8.098-11.255-15.282c0-8.835,7.163-15.999,15.998-15.999c6.004,0,11.229,3.312,13.965,8.204c0.664-0.114,1.338-0.205,2.033-0.205c6.627,0,11.999,5.371,11.999,11.999C71.999,60.268,66.626,65.641,59.999,65.641z"></path>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>
-                        <div>
-                            <ul class="clock" id="clock">
-                                <li class="hour" id="hour"></li>
-                                <li class="min" id="min"></li>
-                                <li class="sec" id="sec"></li>
-                            </ul>
-                            <div class="date f-24 mb-2" id="date"><span id="monthDay"></span><span id="year">, </span></div>
-                            <div>
-                                <p class="m-0 f-14 text-light">Pakistan</p>
-                            </div>
-                        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="slideshow-container">
+                    <?php for($i=1;$i<=10;$i++){ ?>
+                    <div class="mySlides fade">
+                        <div class="numbertext"><?=$i?> / 3</div>
+                        <img src="{{asset('public/slider_imgs/i'.$i.'.jpeg')}}" style="width:100%">
+                        <div class="text">Caption Text</div>
                     </div>
+                        <?php }?>
+
+                </div>
+                <br>
+
+                <div style="text-align:center">
+                    <?php for($i=1;$i<=10;$i++){ ?>
+                    <span class="dot"></span>
+                        <?php }?>
                 </div>
             </div>
+        </div>
 
-            <div class="col-xl-4 col-lg-12  calendar-sec box-col-12">
-                <div class="card myheight gradient-primary o-hidden">
+        <div class="row">
+            <div class="col-sm-12">
+                 <div class="card">
+                    <div class="card-header">
+                        <h5>Responsive Example</h5>
+                    </div>
                     <div class="card-body">
-                        <div class="setting-dot">
-                            <div class="setting-bg-primary date-picker-setting position-set pull-right"><i
-                                    class="fa fa-spin fa-cog"></i></div>
+                        <div class="owl-carousel owl-theme" id="owl-carousel-2"  >
+                            <?php for($i=1;$i<=10;$i++){ ?>
+                            <div class="item"><img class="owlCarouselimg img-responsive img-fluid" src="{{asset('public/slider_imgs/i'.$i.'.jpeg')}}"  alt=""></div>
+                            <?php }?>
                         </div>
-                        <div class="default-datepicker">
-                            <div class="datepicker-here" data-language="en"></div>
-                        </div>
-                        <span class="default-dots-stay overview-dots full-width-dots"><span class="dots-group"><span
-                                    class="dots dots1"></span><span class="dots dots2 dot-small"></span><span
-                                    class="dots dots3 dot-small"></span><span class="dots dots4 dot-medium"></span><span
-                                    class="dots dots5 dot-small"></span><span class="dots dots6 dot-small"></span><span
-                                    class="dots dots7 dot-small-semi"></span><span
-                                    class="dots dots8 dot-small-semi"></span><span
-                                    class="dots dots9 dot-small">                </span></span></span>
                     </div>
                 </div>
             </div>
-
-
-
         </div>
     </div>
     <script type="text/javascript">
@@ -126,33 +191,67 @@
     </script>
 @endsection
 
+<script src="{{asset('public/js/swiper.min.js')}}"></script>
+{{--<script src="{{asset('public/js/ext-component-swiper.js')}}"></script>--}}
 @section('script')
-    <script src="{{asset(config('global.asset_path').'/js/chart/chartist/chartist.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/chart/chartist/chartist-plugin-tooltip.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/chart/knob/knob.min.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/chart/knob/knob-chart.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/chart/apex-chart/apex-chart.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/chart/apex-chart/stock-prices.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/notify/bootstrap-notify.min.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/dashboard/default.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/notify/index.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/datepicker/date-picker/datepicker.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/datepicker/date-picker/datepicker.en.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/datepicker/date-picker/datepicker.custom.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/typeahead/handlebars.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/typeahead/typeahead.bundle.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/typeahead/typeahead.custom.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/typeahead-search/handlebars.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/typeahead-search/typeahead-custom.js')}}"></script>
-
-
-    <script src="{{asset(config('global.asset_path').'/js/prism/prism.min.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/clipboard/clipboard.min.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/counter/jquery.waypoints.min.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/counter/jquery.counterup.min.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/counter/counter-custom.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/custom-card/custom-card.js')}}"></script>
     <script src="{{asset(config('global.asset_path').'/js/owlcarousel/owl.carousel.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/general-widget.js')}}"></script>
-    <script src="{{asset(config('global.asset_path').'/js/height-equal.js')}}"></script>
+    <script>
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 2000); // Change image every 2 seconds
+        }
+
+
+
+        $(document).ready(function (){
+
+            // autoplay
+            var mySwiper10 = new Swiper('.swiper-autoplay', {
+                spaceBetween: 30,
+                centeredSlides: true,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }
+            });
+
+
+
+           $('#owl-carousel-2').owlCarousel({
+                loop:true,
+                autoplay:true,
+                autoplayTimeout:2000,
+                autoplayHoverPause:true,
+                margin:10,
+                items:1,
+                nav:false
+            })
+        });
+    </script>
+
+
+
 @endsection
